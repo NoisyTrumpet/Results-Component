e909918af5ad637d42aa872ec5dba7c3b1f706d7
Cesar Benavides <cesarbenavides@icloud.com>
2020-05-05 11:39:20 -0500

    Custom React Component for Case Studies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CountUp from 'react-countup';
+
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="resultsWrapper">
+      <h2 id="title">Results</h2>
+      <div className="stats">
+        <article>
+          <h2>
+          <CountUp
+          end={1.56}
+          decimals={2}
+          duration={5}
+          start={0}
+          suffix={"B"}
+          useEasing={true}
+          />
+          </h2>
+          <h3>Impressions</h3>
+        </article>
+        <article>
+          <h2>
+          <CountUp
+          end={400}
+          duration={5}
+          suffix={"K"}
+          useEasing={true}
+          />
+          </h2>
+          <h3>Engagements</h3>
+        </article>
+        <article>
+          <h2>
+          <CountUp
+          end={93.5}
+          decimals={1}
+          duration={5}
+          suffix={"K"}
+          useEasing={true}
+          />
+          </h2>
+          <h3>Clicks</h3>
+        </article>
+      </div>
+      </div>
     </div>
   );
 }
